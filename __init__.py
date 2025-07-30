471497d0901a10295ad9a5cc172e199df1d799a3
Sulaiman <5022721+suli-g@users.noreply.github.com>
2025-07-30 19:06:26 +0200

    Initial commit.

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+Implements packages to simplify representation
+of NumPy arrays using LaTeX syntax.
+
+Packages:
+    - `latex_array` implements classes for representing
+      arrays in LaTeX format.
+    - `utils` provides utility functions for working
+      with arrays.
+"""
