a794b73a0bdace0121be82fa669b4011dcad7c01
Sulaiman <5022721+suli-g@users.noreply.github.com>
2025-07-30 23:35:26 +0200

    Add tests for vector functions.

---
+++ tests/compile_vectors_test.py
@@ -0,0 +1,40 @@
+import unittest
+import numpy as np
+from utils.compile import compile_to_latex
+
+
+class TestCompileVectorsToLatex(unittest.TestCase):
+    """
+    Test class for compiling vectors to LaTeX,
+    """
+
+    ROW_VECTOR = r"""
+$$
+\begin{pmatrix}
+1 & 2 & 3
+\end{pmatrix}
+$$
+"""
+
+    COL_VECTOR = r"""
+$$
+\begin{pmatrix}
+1 \\
+3
+\end{pmatrix}
+$$
+"""
+
+    def test_compile_row_vector(self):
+        """
+        Test to make sure that row-vectors compile correctly.
+        """
+        test = np.array([1, 2, 3])
+        self.assertEqual(compile_to_latex(test), self.ROW_VECTOR)
+
+    def test_compile_column_vector(self):
+        """
+        Test to make sure that column-vectors compile correctly.
+        """
+        test = np.array([[1], [3]])
+        self.assertEqual(compile_to_latex(test), self.COL_VECTOR)
