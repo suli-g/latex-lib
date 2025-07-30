6b4c8499aaeb2799ecf730a92826706906317701
Sulaiman <5022721+suli-g@users.noreply.github.com>
2025-07-31 00:36:13 +0200

    Get tests to pass.

--- tests/compile_matrix_test.py
+++ tests/compile_matrix_test.py
@@ -4,19 +4,50 @@ from utils.compile import compile_to_latex
 
 
 class TestCompileMatrixToLatex(unittest.TestCase):
-    MATRIX_3_BY_3 = r"""
+    MATRIX_3_BY_4 = r"""
 $$
 \begin{bmatrix}
-1 & 2 & 3 \\
-4 & 5 & 6 \\
-7 & 8 & 9
+1 & 2 & 3 & 4 \\
+5 & 6 & 7 & 8 \\
+9 & 10 & 11 & 12
 \end{bmatrix}
 $$
 """
 
-    def test_compile_3_by_3_matrix(self):
+    MATRIX_3_BY_2_BY_2 = r"""
+$$
+\begin{bmatrix}
+\begin{pmatrix}
+1 & 2
+\end{pmatrix} & \begin{pmatrix}
+3 & 4
+\end{pmatrix} \\
+\begin{pmatrix}
+5 & 6
+\end{pmatrix} & \begin{pmatrix}
+7 & 8
+\end{pmatrix} \\
+\begin{pmatrix}
+9 & 10
+\end{pmatrix} & \begin{pmatrix}
+11 & 12
+\end{pmatrix}
+\end{bmatrix}
+$$
+"""
+    VALUES = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+    def test_compile_3_by_4_matrix(self):
+        """
+        Test to make sure that 3-by-4 matrices compile correctly.
+        """
+        self.assertEqual(
+            compile_to_latex(self.VALUES.reshape(3, 4)), self.MATRIX_3_BY_4
+        )
+
+    def test_compile_3_by_2_by_2_matrix(self):
         """
         Test to make sure that 3-by-3 matrices compile correctly.
         """
-        test = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-        self.assertEqual(compile_to_latex(test), self.MATRIX_3_BY_3)
+        test = self.VALUES.reshape((3, 2, 2))
+        self.assertEqual(compile_to_latex(test), self.MATRIX_3_BY_2_BY_2)
